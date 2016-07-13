ff8b507fd325bc0a074eff9c1e9e91f598cabc6e
alanxi2322 <xilun19890331@outlook.com>
2016-07-13 14:03:45 -0400

    World.java

---
+++ World.java
@@ -0,0 +1,22 @@
+/*
+ * CS 1632 Deliverable 5 
+ * @ Lun Xi 
+ *
+ */
+package conwaygame;
+
+
+import java.util.*;
+
+
+public class World  {
+
+    public World(int size) {
+	
+    }
+
+    public World(ArrayList<String> lines) {
+
+    }
+
+}
