5e3812f8a529f348280303f5a8c46ed872a023bc
alanxi2322 <xilun19890331@outlook.com>
2016-07-13 14:00:53 -0400

    LoadButton.java

---
+++ LoadButton.java
@@ -0,0 +1,33 @@
+/*
+ * CS 1632 Deliverable 5 
+ * @ Lun Xi 
+ *
+ */
+package conwaygame;
+
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class LoadButton extends JButton {
+
+    private MainPanel _m;
+    
+    public LoadButton(MainPanel m) {
+	super("Load");
+	_m = m;
+	addActionListener(new LoadButtonListener());
+    }
+
+    class LoadButtonListener implements ActionListener {
+
+	public void actionPerformed(ActionEvent e) {
+	    String fileName = "backup.txt";
+	    ArrayList<String> info = FileAccess.loadFile(fileName);
+	    _m.load(info);
+	}
+    }    
+    
+}
